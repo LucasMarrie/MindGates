2c3b4b46bc0582416487341418f21e1dbcdc9a6a
Lucas <lucas.marrie.21@ucl.ac.uk>
2022-03-25 10:30:13 +0000

    change paths issue

--- src/settings.py
+++ src/settings.py
@@ -3,14 +3,14 @@ import os
 #Base Paths
 dirname = os.path.dirname(__file__)
 
-ASSETS_PATH = os.path.join(dirname, '../assets')
-DATA_PATH = os.path.join(dirname, '../data')
+ASSETS_PATH = os.path.join(dirname, '..', 'assets')
+DATA_PATH = os.path.join(dirname, '..', 'data')
 
 #Paths
 LOGICGATE_IMAGE_PATH = os.path.join(ASSETS_PATH, 'logic_gates')
 BUTTON_IMAGE_PATH = os.path.join(ASSETS_PATH, 'buttons')
-FONT_PATH = os.path.join(ASSETS_PATH, 'fonts/Endless Boss Battle.ttf')
-FONT_PATH_TK = os.path.join(ASSETS_PATH, 'fonts/Endless Boss Battle')
+FONT_PATH = os.path.join(ASSETS_PATH, 'fonts', 'Endless Boss Battle.ttf')
+FONT_PATH_TK = os.path.join(ASSETS_PATH, 'fonts', 'Endless Boss Battle')
 
 GRIDDATA_FILE = os.path.join(DATA_PATH, 'gridData.json')
 SCOREDATA_FILE = os.path.join(DATA_PATH, 'scores.json')
